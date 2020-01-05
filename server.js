e8064cd3924bd03f97d8502e5b9e3a796eabcaf5
Roy Peled <peled.roy@gmail.com>
2020-01-05 20:46:00 +0200

    added restful api endpoints

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-const messageHistory = require('./history.json') || [];
+let messageHistory = require('./history.json') || [];
+
+app.use(express.json());
+
+function saveHistory() {
+    fs.writeFileSync('history.json', JSON.stringify(messageHistory), {encoding: 'UTF-8'});
+}
 
 app.get('/', (req, res) => {
     res.send(
@@ -10,13 +16,32 @@ app.get('/', (req, res) => {
     )
 });
 
-app.get('/create-message', (req, res) => {
-    messageHistory.push(req.query);
-    fs.writeFileSync('history.json', JSON.stringify(messageHistory), {encoding: 'UTF-8'});
+app.post('/messages', (req, res) => {
+    messageHistory.push({
+        ...req.body,
+        id: messageHistory.length + 1,
+    });
+    saveHistory();
     res.send(messageHistory);
 });
 
-app.get('/read-messages', (req, res) => {
+app.get('/messages', (req, res) => {
+    res.send(messageHistory);
+});
+
+app.delete('/messages', (req, res) => {
+    messageHistory = [];
+    saveHistory();
+    res.send(messageHistory);
+});
+
+app.get('/messages/:id', (req, res) => {
+    res.send(messageHistory.find(item => item.id == req.params.id));
+});
+
+app.delete('/messages/:id', (req, res) => {
+    messageHistory = messageHistory.filter(item => item.id != req.params.id);
+    saveHistory();
     res.send(messageHistory);
 });
 
